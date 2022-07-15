3336817a71037a28c0d01b1e2a7cc5abe22ab841
valbuko <Valery.Buko@ah.nl>
2022-07-15 15:06:38 +0200

    created product service based on lambda functions

---
+++ product-service/src/functions/hello/handler.ts
@@ -0,0 +1,16 @@
+import type { ValidatedEventAPIGatewayProxyEvent } from '@libs/api-gateway';
+import { formatJSONResponse } from '@libs/api-gateway';
+import { middyfy } from '@libs/lambda';
+
+import schema from './schema';
+
+const hello: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (
+  event
+) => {
+  return formatJSONResponse({
+    message: `Hello AWS and Lambda with API Gateway, welcome to the exciting Serverless world!`,
+    event,
+  });
+};
+
+export const main = middyfy(hello);

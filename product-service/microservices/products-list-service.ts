f558d00de744276e18387f486bd2637174bafe5c
valbuko <Valery.Buko@ah.nl>
2022-07-28 18:39:54 +0200

    [task-4] created getProductsById method for ProductsListService

--- product-service/microservices/products-list-service.ts
+++ product-service/microservices/products-list-service.ts
@@ -1,5 +1,5 @@
 import { Client } from 'pg';
-import { getProductsListQuery } from '../queries/index';
+import { getProductsListQuery, getProductsByIdQuery } from '../queries/index';
 
 class ProductsListService {
   constructor(private pgClient: Client) {
@@ -8,7 +8,12 @@ class ProductsListService {
 
   async getProductsList() {
     const result = await this.pgClient.query(getProductsListQuery);
-    return result.rows ?? 'SQL Query Error';
+    return result.rows ?? 'SQL Query Error, getProductsList method';
+  }
+
+  async getProductsById(id: string) {
+    const result = await this.pgClient.query(getProductsByIdQuery, [id]);
+    return result.rows ?? 'SQL Query Error, getProductsById method';
   }
 }
 
--- product-service/queries/index.ts
+++ product-service/queries/index.ts
@@ -1,2 +1,4 @@
 export const getProductsListQuery: string =
   'SELECT id, count, price, title, description FROM products INNER JOIN stocks ON products.id = stocks.product_id;';
+export const getProductsByIdQuery: string =
+  'select id, count, price, title, description from products inner join stocks on products.id = stocks.product_id and id=$1';

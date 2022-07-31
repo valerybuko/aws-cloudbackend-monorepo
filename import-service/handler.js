21bb988be4f5c27e6186bcd86d294d5273616ece
valbuko <Valery.Buko@ah.nl>
2022-07-31 13:40:18 +0200

    created importFileParser lambda

--- import-service/handler.js
+++ import-service/handler.js
@@ -1,4 +1,5 @@
 const AWS = require('aws-sdk');
+const csv = require('csv-parser');
 const BUCKET = 'import-service-cloudfront';
 
 module.exports = {
@@ -24,5 +25,36 @@ module.exports = {
       },
       body: uploadedUrl,
     };
+  },
+  importFileParser: async function (event) {
+    const s3 = new AWS.S3({ region: 'eu-west-1' });
+    const record = event.Records[0];
+    const params = {
+      Bucket: BUCKET,
+      Key: record.s3.object.key,
+    };
+
+    const s3Stream = s3.getObject(params).createReadStream();
+
+    s3Stream
+      .pipe(csv())
+      .on('data', (data) => {})
+      .on('error', (error) => {})
+      .on('end', async () => {
+        await s3
+          .copyObject({
+            Bucket: BUCKET,
+            CopySource: `${BUCKET}/${record.s3.object.key}`,
+            Key: record.s3.object.key.replace('uploaded', 'parsed'),
+          })
+          .promise();
+
+        await s3
+          .deleteObject({
+            Bucket: BUCKET,
+            Key: record.s3.object.key,
+          })
+          .promise();
+      });
   },
 };
